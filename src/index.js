b8620e7fa7a47839d9d7507c9e245d477a54abe8
donkeeman <doonkeeemaan@gmail.com>
2024-05-13 01:49:09 +0900

    delete unused library

--- src/index.js
+++ src/index.js
@@ -1,15 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { AptosWalletAdapterProvider } from '@aptos-labs/wallet-adapter-react';
-import { PetraWallet } from 'petra-plugin-wallet-adapter';
 const root = ReactDOM.createRoot(document.getElementById('root'));
-const wallets = [new PetraWallet()]
 
 root.render(
   <React.StrictMode>
-    <AptosWalletAdapterProvider plugins={wallets} autoConnect={true}>
         <App />
-    </AptosWalletAdapterProvider>
   </React.StrictMode>
 );
